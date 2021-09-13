8073555e47113152b6adcfed2e8664d096d764b3
yahassane <84969138+Yahassane-learn@users.noreply.github.com>
2021-09-13 10:08:14 +0000

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import acceuil from './acceuil.png' ;
 function Home() {
     return (
         <div>
             <h1 className="wlmv"> Welcome in Our MovieCard</h1>
             <h5 className="wlmv"> Please click on WatchList to see the List Movies</h5>
+            <img className='acceuil' src={acceuil} alt='acceuil' />
         </div>
     )
-
 }
 export default Home;
